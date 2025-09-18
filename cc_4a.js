3db220b0cc53467ad795233ef9f894f83f34b8b3
Isabella Jones <injones@usf.edu>
2025-09-18 12:35:57 -0400

    Initial Commit step 1 and 2 completed

---
+++ cc_4a.js
@@ -0,0 +1,13 @@
+// Shirts, Shoes, Plates, Mugs, TV
+let products = [{
+    name: "Shirt", category: "apparel", price: 35.99, inventort: 50
+},{
+    name: "Shoes", category: "apparel", price: 44.00, inventort: 100
+},{
+    name: "Plates", category: "kitchen", price: 15.00, inventort: 130
+},{
+    name: "Mugs", category: "kitchen", price: 6.99, inventort: 121
+},{
+    name: "TV", category: "electronics", price: 90.00, inventort: 40
+}];
+
