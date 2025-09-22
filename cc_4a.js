0fb85736a3aa7c5fe529a3a23d515fed85052f95
Isabella Jones <injones@usf.edu>
2025-09-21 22:51:18 -0400

    Initial Commit comments made throughout the code to help explain it better

--- cc_4a.js
+++ cc_4a.js
@@ -1,5 +1,5 @@
-// Shirts, Shoes, Plates, Mugs, TV
-let products = [{
+//Step 2// Product List including categories, price, and inventory 
+let products = [{ // shirts, Shoes, Plates, Mugs, TV
     name: "Shirt", category: "apparel", price: 35.99, inventory: 50
 },{
     name: "Shoes", category: "apparel", price: 44.00, inventory: 100
@@ -11,39 +11,42 @@ let products = [{
     name: "TV", category: "electronics", price: 90.00, inventory: 40
 }];
 
-// Displaying all of the products
+// Displaying all of the products for the console 
 console.log(`----------- Products ----------------`)
 products.forEach(products => console.log (
     `${products.name} | ${products.category} | $${products.price} each | ${products.inventory} inv.`
 ));
 
-//---------------------------------------------------
+//-------------------------------------------------------------------//
+//Step 3// Creating a for of loop including a switch
 for (let product of products) {
     let discounts = 0;
 
     switch (product.category) {
         case "apparel":
-            discounts = 0.15
+            discounts = 0.15 //15%
             break;
         case "kitchen":
-            discounts = 0.10
+            discounts = 0.10 //10%
             break;
         case "electronics":
-            discounts = 0.20
+            discounts = 0.20 //20%
             break;
         default:
-            discounts = 0;
+            discounts = 0; //if it doesnt fall into any of the categories its 0%
     }
 
-    let promoPrice = product.price - (product.price* discounts);
-    product.promoPrice = promoPrice.toFixed(2)
+    let promoPrice = product.price - (product.price* discounts); //a formula used to calculate the price of an item after discounts
+    product.promoPrice = promoPrice.toFixed(2) //making sure that the new price is added into the arrary as well as fixed to 2 decimals
 }
+//Displaying the promo price for all of the products
 console.log(`------------ Promo Price --------------`)
 products.forEach(products => console.log (
     `${products.name} | $${products.promoPrice}`
 ));
-// Everything works from here and above
-console.log(`-------------------------------------------`);
+
+//Step 5// Create a customer cart loop
+console.log(`-------------------------------------------`); // a way to break up the log
 
 let customers = [
     {id: 1, type: "regular", 
@@ -56,18 +59,18 @@ let customers = [
     {id: 3, type: "senior", 
         cart: [{name: "TV", quantity: 2},
             {name: "Mugs", quantity: 10}]}
-];
+]; // so far the variable names used is: name, quantity, cart, type, and id
 
 for (let customer of customers) {
-    console.log(`--- Customer ID_${customer.id} (${customer.type})---`);
+    console.log(`--- Customer ID_${customer.id} (${customer.type})---`); //creating a heading for each of the customers carts
  
-let subtotal = 0;
+let subtotal = 0;//important and said to be used from the hints
 
 for (let item of customer.cart) {
-    let product = products.find(product => product.name === item.name)
+    let product = products.find(product => product.name === item.name) //use find to look through the products, then rename them to item
 
-    if (product && product.inventory >= item.quantity) {
-        let itemTotal = parseFloat(product.promoPrice) * item.quantity
+    if (product && product.inventory >= item.quantity) { //making sure that the product exist and that there is enough inventory
+        let itemTotal = parseFloat(product.promoPrice) * item.quantity // need to switch promoPrice from string to number by using parsefloat
         subtotal += itemTotal // adding the item total of the cart to the subtotal
         product.inventory -= item.quantity //subtracting the item quantity from the cart to the stock of that item
 
@@ -78,39 +81,43 @@ for (let item of customer.cart) {
         }
 }
 
+//Listing the subtotal before the customer type discounts are applied
 console.log(`Subtotal before discounts: $${subtotal.toFixed(2)}`);
 
-
-if (customer.type === "regular") {
+//Step 4// Customer type discounts are created using an if...else statement
+if (customer.type === "regular") { //technically I can get rid of this since its the same as else but Id rather list it just in case
     customerDiscounts = 0; }
-    else if (customer.type === "student") {
-        customerDiscounts = 0.05; }
+    else if (customer.type === "student") {  // use customer.type instead of creating a new variable
+        customerDiscounts = 0.05; } // 5%
     else if (customer.type === "senior") {
-        customerDiscounts = 0.08; }
+        customerDiscounts = 0.08; } // 8%
     else {
         customerDiscounts = 0; }
 
 
-let discountAmount = subtotal * customerDiscounts;
-total = subtotal - discountAmount
+let discountAmount = subtotal * customerDiscounts; //calculating the total using the discounts
+total = subtotal - discountAmount //the total!!
 
+//displaying the total for every customer id
 console.log(`Total after discounts: $${total.toFixed(2)}`);
 }
 
 
 
-//Updated Stock
+//Updated Stock// Listing out all of the products stock after what was bought by customers
 console.log(`----------- Updated Stock ----------------`)
 products.forEach(products => console.log (
     `${products.name} | ${products.inventory} inv.`
 ));
 
+//Step 6// listing out the keys for a product created in Step 2
 console.log("------ Keys/Values for a Product ----");
 let productKey = products[0];
 for (let key in productKey) {
     console.log(`${key}: ${productKey[key]}`);
 }
 
+//Step 7// Just listing out all of the products again like I did for Step 2 but now updated with everything that was done above
 console.log("------------ All Products After Updates --------");
 for (let {name, category, price, promoPrice, inventory } of products) {
     console.log(`${name} | ${category} | Org: $${price.toFixed(2)} | Promo: $${promoPrice} | Inv: ${inventory}`);
