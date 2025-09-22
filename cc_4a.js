ac3d75bfe8cdd991e4d401b983684b9d504ffd06
Isabella Jones <injones@usf.edu>
2025-09-21 22:33:34 -0400

    Inital Commit step 5 through 7 completed

--- cc_4a.js
+++ cc_4a.js
@@ -14,7 +14,7 @@ let products = [{
 // Displaying all of the products
 console.log(`----------- Products ----------------`)
 products.forEach(products => console.log (
-    `${products.name} | ${products.category} | $${products.price} | ${products.inventory}`
+    `${products.name} | ${products.category} | $${products.price} each | ${products.inventory} inv.`
 ));
 
 //---------------------------------------------------
@@ -43,6 +43,7 @@ products.forEach(products => console.log (
     `${products.name} | $${products.promoPrice}`
 ));
 // Everything works from here and above
+console.log(`-------------------------------------------`);
 
 let customers = [
     {id: 1, type: "regular", 
@@ -55,22 +56,62 @@ let customers = [
     {id: 3, type: "senior", 
         cart: [{name: "TV", quantity: 2},
             {name: "Mugs", quantity: 10}]}
-]
+];
 
 for (let customer of customers) {
     console.log(`--- Customer ID_${customer.id} (${customer.type})---`);
+ 
+let subtotal = 0;
+
+for (let item of customer.cart) {
+    let product = products.find(product => product.name === item.name)
+
+    if (product && product.inventory >= item.quantity) {
+        let itemTotal = parseFloat(product.promoPrice) * item.quantity
+        subtotal += itemTotal // adding the item total of the cart to the subtotal
+        product.inventory -= item.quantity //subtracting the item quantity from the cart to the stock of that item
+
+        //logging the cart into the console plus some extra details for if the inventory is low 
+        console.log(`${product.name} | $${product.promoPrice} x ${item.quantity} = $${itemTotal.toFixed(2)}`);
+            } else {
+                console.log(`!Low Inventory! *${product.name}* | Available: ${product.inventory} | Wanted: ${item.quantity}`);
+        }
 }
 
-let customerType = ["regular", "student", "senior"];
+console.log(`Subtotal before discounts: $${subtotal.toFixed(2)}`);
 
-if (customerType === "regular") {
+
+if (customer.type === "regular") {
     customerDiscounts = 0; }
-    else if (customerType === "student") {
+    else if (customer.type === "student") {
         customerDiscounts = 0.05; }
-    else if (customerType === "senior") {
+    else if (customer.type === "senior") {
         customerDiscounts = 0.08; }
     else {
         customerDiscounts = 0; }
 
-// Id, 
-let checkOutCart = [{}]
+
+let discountAmount = subtotal * customerDiscounts;
+total = subtotal - discountAmount
+
+console.log(`Total after discounts: $${total.toFixed(2)}`);
+}
+
+
+
+//Updated Stock
+console.log(`----------- Updated Stock ----------------`)
+products.forEach(products => console.log (
+    `${products.name} | ${products.inventory} inv.`
+));
+
+console.log("------ Keys/Values for a Product ----");
+let productKey = products[0];
+for (let key in productKey) {
+    console.log(`${key}: ${productKey[key]}`);
+}
+
+console.log("------------ All Products After Updates --------");
+for (let {name, category, price, promoPrice, inventory } of products) {
+    console.log(`${name} | ${category} | Org: $${price.toFixed(2)} | Promo: $${promoPrice} | Inv: ${inventory}`);
+}
